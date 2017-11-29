e2815bf73668d86e75117a6151918487b0af665f
yves <461836324@qq.com>
2017-11-29 17:42:24 +0800

    修改页面

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,8 @@
 <template>
   <header class="app-header" :style="'background-color: ' + data.hex">
     <h1 class="app-header-title" :style="'color: ' + (data.isDepth ? '#fff' : '#000')">传统色彩{{ data.name ? ('-' + data.name) : '' }}</h1>
+    <span class="app-header-title-hex" :style="'color: ' + (data.isDepth ? '#fff' : '#000')">{{ data.hex }}</span>
+    <span class="app-header-title-rgb" :style="'color: ' + (data.isDepth ? '#fff' : '#000')">{{ data.rgb }}</span>
   </header>
 </template>
 
@@ -24,15 +26,25 @@ export default {
   height: 64px
   background-color: currentColor
   box-shadow: 0 1px 1px 0 currentColor, 0 2px 1px -2px currentColor, 0 1px 3px 0 currentColor
-  transition: background-color 300ms
-  user-select: none
+  transition: background-color 300ms, color 300ms
   .app-header-title
     font-weight: 400
     font-size: 20px
     line-height: 64px
     position: relative
     z-index: 1
+    user-select: none
     margin-left: 20px
-    transition: color 300ms
-    color: #000000
+  span
+    position: absolute
+    right: 20px
+    display: block
+    text-align: right
+    width: 200px
+    line-height: 26px
+    z-index: 10
+    &.app-header-title-hex
+      top: 8px
+    &.app-header-title-rgb
+      bottom: 8px
 </style>
